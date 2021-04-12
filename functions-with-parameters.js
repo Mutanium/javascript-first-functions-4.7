9c2580b5a16e2959fcd13dd2ea4776bd8a6f858e
Mutanium <info@gaetan.nl>
2021-04-12 13:16:42 +0200

    opdracht 4.7 ed hub

--- functions-with-parameters.js
+++ functions-with-parameters.js
@@ -7,15 +7,21 @@
 function add(a, b) {
   return a + b;
 }
-
 const result = add(1, 4);
 console.log(result);
+
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd greeter, die een naam verwacht en een groet teruggeeft
 // ---- Verwachte uitkomsten:
 // greeter("Jan") geeft "Hoi Jan!"
-// greeter("Kees") geeft "Hoi Kees!"
+// greeter("Kees") geef "Hoi Kees!"
+
+function greeter (name) {
+  return "Hoi "+ name;
+}
+
+console.log(greeter("Henk"));
 
 
 
@@ -24,7 +30,23 @@ console.log(result);
 // ---- Verwachte uitkomsten:
 // minutesToSeconds(1) geeft 60
 // minutesToSeconds(3) geeft 180
-// minutesToSeconds(23) geeft 1380
+// minutesToSeconds(23) geft 1380
+
+
+function minToSec (minutes){
+  return minutes * 60;
+}
+
+for (minutes=1; minutes<11; minutes++) {
+
+  if(minutes === 1) {
+    console.log(minutes + " minuut bestaat uit " + minToSec(minutes) + " seconden.");
+  }
+  else {
+    console.log(minutes + " minuten bestaan uit " + minToSec(minutes) + " seconden.");
+  }
+}
+
 
 
 
@@ -35,6 +57,13 @@ console.log(result);
 // merge("abra", "cadabra") geeft "abracadabra"
 // merge("zoet", "sappig") geeft "zoetsappig"
 
+function merge (first,last){
+  return first + last;
+}
+const total = merge("abra", "cadabra");
+console.log(total);
+
+
 
 
 
@@ -44,15 +73,32 @@ console.log(result);
 // calculateDogYears(6) geeft "Jouw hond is 42 jaar oud in mensenjaren."
 // calculateDogYears(2) geeft "Jouw hond is 14 jaar oud in mensenjaren."
 
+function calculateDogYears (dogAge) {
+  return dogAge * 7
+}
+const animalAge = 7
+const humanAge = calculateDogYears(animalAge);
+console.log("Een hond van " + animalAge + " jaar oud, is vergelijkbaar met een mens van " + humanAge + " jaar oud.");
 
 
-/* Opdracht 5 */
+/* Opdacht 5 */
 // Schrijf een functie genaamd wrapper, die twee parameters verwacht: een woord en een karakter
 // De functie omwikkelt het woord met het karakter en geeft dit terug
 // ---- Verwachte uitkomsten:
 // wrapper("bril", "*") geeft "*bril*"
 // wrapper("beep", "_") geeft "_beep_"
 // wrapper("kaas", "Q") geeft "QkaasQ"
+
+function wrapper (word, character) {
+  return character + word + character;
+}
+
+// de wrapper gedeclareerd met twee namen als een nieuwe variabele: wrapped
+const wrapped = wrapper("sterretjes", "*");
+console.log(wrapped);
+
+//de wrapper pas gedeclareerd in de console log
+console.log(wrapper("sterretjes", "*"));
 
 
 
@@ -63,3 +109,9 @@ console.log(result);
 // createDetailString({ firstName: 'Kees', lastName: 'Klaasen', profession: 'brandweerman'}) geeft "Het beroep vam Kees Klaasen is brandweerman."
 
 
+function createDetailString (firstName, lastName, profession) {
+  const fullName = firstName + " " + lastName;
+  return "Het beroep van " + fullName + " is " + profession
+}
+const feedBack = createDetailString("Gaetan", "Asselberghs", "Programmeur");
+console.log(feedBack);
--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 /* Opdracht 0: voorbeeld */
 // Schrijf een functie die de string "Goedemorgen" teruggeeft
-// ---- Verwachte uitkomst bij het aanroepen van de functie: "Goedemorgen"
+// ---- Verwacht uitkomst bij het aanroepen van de functie: "Goedemorgen"
 
 function morningGreeter() {
   return "Goedemorgen";
@@ -14,11 +14,30 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function institute() {
+  return "Novi Hogeschool";
+}
+//loggen via aangemaakte variabele
+const school = institute();
+console.log(school);
+//of loggen door de functie rechtstreeks in de log te stoppen
+console.log(institute());
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+
+function compliment(name) {
+  return name + " je bent un kanon!"
+}
+const name = "Gaetan";
+console.log(compliment(name));
+
+function compliment2 (name2) {
+  return name2 + " dat heb je goed gedaan!"
+}
+console.log(compliment2("Henk"));
 
 
 
@@ -26,8 +45,31 @@ console.log(greeting);
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8, 8.5, 6, 7];
+}
+
+console.log(getGrades());
+
+const grades = getGrades();
+console.log(grades);
+
+
 
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+
+function details(firstname, lastname) {
+  return "voornaam: " + firstname + " achternaam: " + lastname;
+}
+
+const firstname = "Gaetan";
+const lastname = "Asselberghs";
+console.log(details(firstname, lastname));
+
+//of door een nieuwe variabele te maken:
+const output = details(firstname, lastname);
+console.log(output);
